8c48c9dd69dd3effd577880394f36f25ff0cc529
Aluno <Aluno@DESKTOP-LDNVJ6K>
2018-08-06 08:09:09 -0300

    Projeto inicial referente ao nosso TCC

---
+++ TCC2/src/main/java/BasicoBean.java
@@ -0,0 +1,107 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+@ApplicationScoped
+
+public class BasicoBean {
+
+	private List<Redacao> redacoes = new ArrayList<Redacao>();
+
+	private int id;
+	private Redacao redacao;
+	private Redacao autor;
+	private Redacao metodospmelhorar;
+
+	public Redacao getMetodospmelhorar() {
+		return metodospmelhorar;
+	}
+
+	public void setMetodospmelhorar(Redacao metodospmelhorar) {
+		this.metodospmelhorar = metodospmelhorar;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public BasicoBean() {
+		redacao = new Redacao();
+	}
+
+	public Redacao getRedacao() {
+		return redacao;
+	}
+
+	public void setAutor(Redacao autor) {
+		this.autor = autor;
+	}
+
+	public List<Redacao> getUsuarios() {
+		return redacoes;
+	}
+
+	public void addRedacao() {
+		redacoes.add(redacao);
+		redacao = new Redacao();
+	}
+
+	public void removeRedacao() {
+		for (Redacao redacao : redacoes) {
+			if (redacao.getId() == getId()) {
+				redacoes.remove(redacao);
+				return;
+			}
+		}
+	}
+				
+				public String buscarRedacao(Integer id) {
+					for (Redacao redacao : redacoes) {
+						if (redacao.getId() == id) {
+							 return redacao.getTexto();
+						}
+					}
+					return null;
+	}
+				public void atualizar(String texto , Integer id) {
+					for (Redacao redacao : redacoes) {
+						if (redacao.getId() == id) {
+							redacao.setTexto(texto);
+							return;
+						}
+					}
+				
+	}
+			
+
+	public List<Redacao> getRedacoes() {
+		return redacoes;
+	}
+
+	public void setRedacoes(List<Redacao> redacoes) {
+		this.redacoes = redacoes;
+	}
+
+	public Redacao getAutor() {
+		return autor;
+	}
+
+	public void setId_redacao(int id) {
+		this.id = id;
+	}
+
+	public void setRedacao(Redacao redacao) {
+		this.redacao = redacao;
+	}
+	
+	
+	
+}
---
+++ TCC2/src/main/java/Redacao.java
@@ -0,0 +1,41 @@
+
+public class Redacao {
+
+	private String texto;
+	private int id;
+	private String autor;
+	private String metodospmelhorar;
+
+	public String getMetodospmelhorar() {
+		return metodospmelhorar;
+	}
+
+	public void setMetodospmelhorar(String metodospmelhorar) {
+		this.metodospmelhorar = metodospmelhorar;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+}
